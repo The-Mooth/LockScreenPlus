299518e709344a38fab421f95735d8f817018cf3
The-Mooth <107499599+The-Mooth@users.noreply.github.com>
2023-06-02 15:18:03 -0700

    adjusting button

--- src/screens/ClockScreen.js
+++ src/screens/ClockScreen.js
@@ -40,21 +40,22 @@ const ClockScreen = () => {
       {time ? <Text style={styles.text}>time works</Text> : null}
       {location ? <Text style={styles.text}>location works</Text> : null}
       {quote ? <Text style={styles.text}>quote works</Text> : null}
-      <Pressable onPress={handleTabPress}>
-        <Text style={styles.text}>Press me</Text>
-      </Pressable>
+    
       <Animated.View
         style={{
           transform: [
             {
               translateY: slideAnim.interpolate({
                 inputRange: [0, 1],
-                outputRange: [0, -100],
+                outputRange: [20, -100],
               }),
             },
           ],
         }}
       >
+        <Pressable onPress={handleTabPress}>
+        <Text style={styles.text}>Press me</Text>
+      </Pressable>
         <Text style={styles.text}>Tab</Text>
       </Animated.View>
     </View>

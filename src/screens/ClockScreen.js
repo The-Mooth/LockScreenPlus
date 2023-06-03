cd7a5ee853e69ca00da2f8a93645576fbabef307
The-Mooth <107499599+The-Mooth@users.noreply.github.com>
2023-06-02 17:27:38 -0700

    finished adding svgs

--- src/screens/ClockScreen.js
+++ src/screens/ClockScreen.js
@@ -14,6 +14,7 @@ import {MyText, MyTextBold} from "../util/MyText";
 //components
 import BottomScreen from "../components/BottomScreen";
 import ArrowUp from "../components/svgs/ArrowUp";
+import ArrowDown from "../components/svgs/ArrowDown";
 
 const ClockScreen = () => {
   //api usestates
@@ -66,6 +67,7 @@ const ClockScreen = () => {
       <View style={styles.topRow}>
       {quote ? <MyTextBold>quote bussin</MyTextBold> : null}
       <ArrowUp/>
+      <ArrowDown/>
 
       </View>
       

3a4dfa6c8326ab0dae96d57f77f998341bca1ba7
The-Mooth <107499599+The-Mooth@users.noreply.github.com>
2023-06-02 20:21:39 -0700

    more bottom display stuff

--- src/components/BottomDisplay.js
+++ src/components/BottomDisplay.js
@@ -16,9 +16,31 @@ const BottomDisplay = ({ time, location, isDay, greeting }) => {
     return null;
   }
 
+  //parse data for display
   const timeData = JSON.parse(time);
   const locationData = JSON.parse(location);
 
+  const now = new Date(timeData.datetime);
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let isAM = true;
+
+  if (hours > 12) {
+    hours = hours - 12;
+    isAM = false;
+  } else if (hours === 0) {
+    hours = 12;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  //logic for slider
   const [showTab, setShowTab] = useState(false);
 
   const slideAnim = useRef(new Animated.Value(0)).current;
@@ -34,25 +56,58 @@ const BottomDisplay = ({ time, location, isDay, greeting }) => {
 
   return (
     <View style={styles.container}>
+
       <View style={styles.row}>
         {isDay ? <Sun /> : <Moon />}
-        <MyText>
-          {greeting + ", IT'S CURRENTLY"}
-        </MyText>
+        <MyText>{greeting + ", IT'S CURRENTLY"}</MyText>
       </View>
-      <View style={styles.row}>
-        <MyTextBold>{timeData.datetime}</MyTextBold>
+
+      <View style={[styles.row, {alignItems: "flex-end",}]}>
+        <MyTextBold style={{fontSize: 80, fontWeight: 700}}>{hours + ":" + minutes}</MyTextBold>
+
+        <View style={styles.column}>
+          <MyText>{isAM ? "AM" : "PM"}</MyText>
+          <MyText>{timeData.abbreviation}</MyText>
         </View>
-        <View style={styles.row}>
-          </View>
-      {time ? <Text style={styles.text}>time works</Text> : null}
-      {location ? <Text style={styles.text}>location works</Text> : null}
-      {isDay ? <Text style={styles.text}>isDay works</Text> : null}
+      </View>
+
+      <View style={styles.row}>
+        <MyText>{"In " + locationData.city + ", " + locationData.region}</MyText>
+      </View>
+
+      <View style={styles.row}>
+      <Pressable style={styles.pressable} onPress={handleTabPress}>
+        <MyText style={styles.text}>{showTab ? "LESS" : "MORE"}</MyText>
+        {showTab ? <ArrowDown/> : <ArrowUp/>}
+      </Pressable>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+
+  column: {
+    flexDirection: "column",
+    width: "20%",
+    marginBottom: 20,
+    marginLeft: 10,
+  },
+
+
+  pressable: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "hsl(0, 0%, 80%)",
+    borderRadius: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingVertical: 5,
+    width: "30%",
+    marginBottom: 20,
+  },
+
   row: {
     flexDirection: "row",
     //justifyContent: "flex-start",
@@ -63,15 +118,16 @@ const styles = StyleSheet.create({
   container: {
     //flex: 1,
     width: "100%",
-    //height: "100%",
+    height: "35%",
     justifyContent: "space-between",
-    alignItems: "center",
+    paddingHorizontal: 15,
+    //alignItems: "center",
   },
 
   text: {
-    fontSize: 24,
-    color: "white",
-    fontWeight: "bold",
+    //fontSize: 24,
+    color: "hsl(0, 0%, 25%)",
+    //fontWeight: "bold",
   },
 });
 
